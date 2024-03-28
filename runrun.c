13828012fdb9da402ccb7b5913940af629e959d1
thaleseqb <thalesquadros2@gmail.com>
2024-03-28 11:11:08 -0300

    dynamic memory allocation

--- runrun.c
+++ runrun.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char** map;
+int lines;
+int columns;
 
 int main() {
-    char map[5][10];
 
-    map[0][0] = '|';
-    map[4][9] = '@';
+    FILE* file;
+    file = fopen("mapa.txt", "r");
+    if (file == 0) {
+        printf("error in reading the file");
+        exit(1);
+    }
 
-    printf("%c %c\n", map[0][0], map[4][9]);
+    fscanf(file, "%d %d", &lines, &columns);
+    printf("lines %d columns %d\n", lines, columns);
+
+    map = malloc(sizeof(char*) * lines);
+    for (int idx = 0; idx < lines; idx++) {
+        map[idx] = malloc(sizeof(char) * (columns+1));
+    }
+
+    for (int idx = 0; idx < 5; idx++) {
+        fscanf(file, "%s", map[idx]);
+    }
+
+    for (int idx = 0; idx < 5; idx++) {
+        printf("%s\n", map[idx]);
+    }
+    
+    fclose(file);
+
+    for (int idx = 0; idx < lines; idx++) {
+        free(map[idx]);
+    }
+
+    free(map);
+
 
 }
